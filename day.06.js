6f1dc83c44534aad85fd2b21fa15b7867a9b958e
Bryan Bush <bryan@dialexa.com>
2020-12-06 11:42:37 -0600

    Fix bug!

--- day.06.js
+++ day.06.js
@@ -3,8 +3,7 @@
 document
   .querySelector("pre")
   .innerText.split("\n\n")
-  .filter((x) => x)
-  .map((x) => new Set(x.split("").filter((x) => x !== "\n")).size)
+  .map((x) => new Set(x.replaceAll("\n", "")).size)
   .reduce((acc, cur) => ((acc += cur), acc), 0);
 
 // Part 2
@@ -12,22 +11,20 @@ document
 document
   .querySelector("pre")
   .innerText.split("\n\n")
-  .filter((x) => x)
-  .map((x) =>
-    x.split("\n").map((x) => {
-      const src = x.split("");
-      const ref = "abcdefghiijklmnopqrstuvwxyz".split("");
+  .map(
+    (x) =>
+      x
+        .split("\n")
+        .filter((x) => x)
+        .map((x) => {
+          const src = x.split("");
+          const ref = "abcdefghijklmnopqrstuvwxyz".split("");
 
-      return parseInt(ref.map((x) => (src.includes(x) ? "1" : "0")).join(""), 2);
-    })
+          return parseInt(ref.map((x) => (src.includes(x) ? "1" : "0")).join(""), 2);
+        })
+        .reduce((acc, cur) => ((acc &= cur), acc), Math.pow(2, 26) - 1)
+        .toString(2)
+        .split("")
+        .filter((x) => Number(x)).length
   )
-  .reduce((acc, cur) => {
-    const and = cur.reduce((acc, cur) => ((acc &= cur), acc), Math.pow(2, 26) - 1);
-
-    acc += and
-      .toString(2)
-      .split("")
-      .filter((x) => x === "1").length;
-
-    return acc;
-  }, 0);
+  .reduce((acc, cur) => ((acc += cur), acc), 0);
